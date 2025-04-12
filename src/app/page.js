8b7680a0b3024f2c1256f7b37baf20573c2a86ca
helciohumberto <helciohumberto@gmail.com>
2025-04-11 21:18:22 -0300

    web3 planta crypto

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,51 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { doLogin } from "@/services/Web3Service";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const {push} = useRouter();
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const [message, setMessage] = useState("");
+
+  function btnLoginClick(){
+    setMessage("Conectando na carteira...aguarde...");
+    doLogin()
+      .then(account => push("/create"))
+      .catch(err => {
+        console.error(err);
+        setMessage(err.message)
+      })
+  }
+  return (
+    <>
+      <div className="container px-4 py-5">
+        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div className="col-6">
+            <img src="/slogan.png" className="d-block mx-lg-auto img-fluid" width={400}/>
+          </div>
+          <div className="col-6">
+            <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Planta Crypto</h1>
+            <p className="lead">PlantaCrypto é uma plataforma de doações descentralizadas que conecta quem quer ajudar com quem planta o futuro. Usando tecnologia blockchain e criptomoedas, o projeto facilita doações diretas e transparentes para pequenos agricultores, iniciativas agroecológicas e projetos rurais sustentáveis.
+<br/> <br/>
+A proposta é unir inovação e solidariedade, tornando o ato de doar mais acessível, seguro e global, ao mesmo tempo em que promove o desenvolvimento agrícola com impacto social.</p>
+            <p className="lead">Autentique-se com sua carteira e crie sua campanha. </p>
+            <p className="lead mb-3">Para doações, use o link da campanha já existente.</p>
+            <div className="d-flex justify-content-start mt-5">
+              <button type="button" className="btn btn-primary btn-lg px-4 me-2 col-12" onClick={btnLoginClick}>
+                <img src="/metamask.png" width={64} className="me-2"/>
+                Conectar com a MetaMask
+              </button>
+            </div>
+            {
+              message
+              ? <div className="alert alert-success p-3 col-12 mt-3" role="alert">{message}</div>
+              : <></>
+            }
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
